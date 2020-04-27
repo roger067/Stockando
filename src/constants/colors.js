695f701eff8b0f21338c5f5babca93ac3c9c328d
roger067 <rosestolato.rogerio@gmail.com>
2020-04-26 23:03:04 -0300

    adding colors and structure

---
+++ src/constants/colors.js
@@ -0,0 +1,6 @@
+export const COLORS = {
+  PINK: "#FFD8D8",
+  WHITE: "#FFF",
+  GRAY: "#292929",
+  GRAY100: "#202020",
+};
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import { Dashboard } from "../container";
+
+const DashboardPage = () => <Dashboard />;
+
+export default DashboardPage;
